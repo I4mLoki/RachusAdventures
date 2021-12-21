090b6ba9e8f869d5f90140804af87a6f774fac11
Inari <rdev.ma@gmail.com>
2021-12-21 17:57:16 +0100

    First commit

---
+++ Source/RachusAdventures/RachusAdventuresGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RachusAdventuresGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RACHUSADVENTURES_API ARachusAdventuresGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
